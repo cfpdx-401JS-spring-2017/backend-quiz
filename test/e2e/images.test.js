03f728eef0069274ccd456817bf0156e96bc21ed
Martin <martinconradpelayo@gmail.com>
2017-05-04 16:15:24 -0700

    Probalby got a 70

--- test/e2e/images.test.js
+++ test/e2e/images.test.js
@@ -20,6 +20,27 @@ describe('images API', () => {
     url: 'foobar.qux'
   };
 
+  function savedImage(image) {
+    return request 
+      .post('api/image')
+      .send(image)
+      .then(res => res.body);
+  }
+
+  it('is a test for a post? ', (image) => {
+    return savedImage(fooImageBar)
+    .then(saved => {
+      assert.ok(saved._id, 'whatever');
+      fooImageBar = saved;
+    })
+    .then(() => {
+      return request 
+        .get(`/api/images/${fooImageBar._id}`);
+    })
+    .then(res => {
+      const image = res.body;
+      assert.deepEqual(image, fooImageBar);
+  })
   
 
 
