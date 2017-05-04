79deeaea9847554cb04b2046d0f435ab8c224d4a
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-04 14:52:21 -0700

    POST /images test failing

--- test/_db.js
+++ test/_db.js
@@ -1,5 +1,5 @@
 process.env.MONGODB_URI = 'mongodb://localhost:27017/images-test';
-require('../../lib/connect');
+require('../lib/connect');
 
 const connection = require('mongoose').connection;
 
---
+++ test/images.test.js
@@ -0,0 +1,25 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('images API', () => {
+
+  before(db.drop);
+
+  let testImage = {
+    title: 'sunset.jpg',
+    description: 'a beautiful sunset',
+    category: 'places',
+    url: 'www.someassets.com/image' 
+  };
+
+  it('POST an image', () => {
+    return request.post('/images')
+      .send(testImage)
+      .then(res => res.body)
+      .then(image => {
+        assert.ok(image._id);
+      });
+  });
+
+});
