86064a5278e1d588c7aa84c94ce9fddec59b8efd
Martin <martinconradpelayo@gmail.com>
2017-05-04 15:52:34 -0700

    MEOW

--- test/e2e/images.test.js
+++ test/e2e/images.test.js
@@ -3,6 +3,9 @@ const request = require('./_request');
 const assert = require('chai').assert;
 
 describe('images API', () => {
+  before(() => {
+    return db.drop();
+  });
   it('GETs all images', () => {
     return request.get('/api/images').then(res => {
       const images = res.body;
