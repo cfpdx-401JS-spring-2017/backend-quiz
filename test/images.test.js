cb16861b00221a607386454e3ca451c722d55784
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-04 14:46:08 -0700

    Added util functions - guess I needed process.env.MONGODB_URI after all for testing

---
+++ test/images.test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const request = require('./util/_request');
+const db = require('./util/_db');
+
+describe('Images API', () => {
+
+  before(db.drop);
+
+  it('testing connection', () => {
+    return request.get('/api/studios')
+    .then(res => {
+      const images = res.body;
+      assert.okay(images, []);
+    });
+  });
+});
