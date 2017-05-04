b994002e15832fc1c6dcc31e28a8cec4970b2e50
Morgan Fogarty <mofo37@gmail.com>
2017-05-04 16:17:46 -0700

    server not working.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 /* eslint no-console: "off" */
 const app = require('./lib/app');
 const http = require('http');
+require('./lib/connect');
 
 const connect = require('./lib/connect');
-const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/pets';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/images';
 connect(dbUri);
 
 const server = http.createServer(app);
