e56b67a34b80977d3a9689abc0622265713e9d19
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-04 15:03:39 -0700

    Might be jumping ahead by doing post and get by id, but let's see

--- test/images.test.js
+++ test/images.test.js
@@ -14,4 +14,23 @@ describe('Images API', () => {
         assert.deepEqual(images, []);
       });
   });
+
+  it('roundtrips a new image', () => {
+    let pomImage = {
+      title: 'Bo the Best Pom',
+      descripton: 'He looks like a teddy bear and is fluffy',
+      category: 'animals',
+      url: 'http://bestpomever.com'
+    };
+
+    return request.post('/api/images')
+    .send(pomImage)
+    .then(res => {
+      pomImage = res.body;
+      return request.get(`/api/images/${pomImage._id}`)
+
+    });
+  });
+
+  // it('returns 400 if required field not included' () => {});
 });
