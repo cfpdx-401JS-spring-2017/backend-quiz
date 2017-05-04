fca15eba21f6310ef83987200a81eb6521a1d0b1
Ivy Patton <lilaivy@gmail.com>
2017-05-04 15:56:49 -0700

    stuck on connection again

--- test/e2e/before-after.js
+++ test/e2e/before-after.js
@@ -1,10 +1,10 @@
-const connect = require('../../lib/connect');
+// const connect = require('../../lib/connect');
 
-let connection = null;
+// let connection = null;
 
-before(() => {
-    return connect('mongodb://localhost:27017/images-test')
-        .then(cn => connection = cn);
-});
+// before(() => {
+//     return connect('mongodb://localhost:27017/images-test')
+//         .then(cn => connection = cn);
+// });
 
-after(() => connection.close());
+// after(() => connection.close());
