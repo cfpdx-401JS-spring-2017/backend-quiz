698e7ca7dcd40cc8cf66947629811dd14d1697a7
nicky evers <livelyevers@gmail.com>
2017-05-04 15:27:25 -0700

    image route written, schema written, basic test written, still failing

--- test/images.test.js
+++ test/images.test.js
@@ -1,4 +1,39 @@
-const db = require('_db');
-const request = require('_request');
+const db = require('./_db');
+const request = require('./_request');
 const assert = require('chai').assert;
 
+describe('images api', () => {
+
+    before(db.drop);
+
+    let testImage = {
+        title: 'juggling monkey',
+        description: 'such talent',
+        category: 'animals',
+        url: 'jugglingmonkey.com/image'
+    };
+
+    function saveImage(image) {
+        return request
+            .post('/images')
+            .send(image)
+            .then(res => res.body);
+    }
+
+    it('roundtrips an image', () => {
+        return saveImage(testImage)
+            .then(saved => {
+                assert.ok(saved._id);
+                testImage = saved;
+            })
+            .then(() => {
+                return request.get(`/images/${testImage._id}`);
+            })
+            .then(res => res.body)
+            .then(got => {
+                assert.deepEqual(got, {
+                    // add fields
+                });
+            });
+    });
+});
